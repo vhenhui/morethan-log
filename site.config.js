d12e258ed0e7468ab0fdaf92960d2e6fa7452fd7
vhenhui <vhenhui@163.com>
2025-03-23 22:49:55 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -19,16 +19,16 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
+    title: "实战",
     description: "welcome to morethan-log!",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
+  link: "study.nalenti.com",
   since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
-  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
+  ogImageGenerateURL: "study.nalenti.com", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
   notionConfig: {
