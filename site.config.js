7b9d661e8304d93d6bb0da477fee795c6fdebb2d
vhenhui <vhenhui@163.com>
2025-03-23 22:15:54 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "nalenti",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    bio: "娜兰蒂 三周还你好头发.",
+    email: "vhenhui@163.com",
+    linkedin: "www.nalenti.com",
+    github: "400-1133-778",
     instagram: "",
   },
   projects: [
